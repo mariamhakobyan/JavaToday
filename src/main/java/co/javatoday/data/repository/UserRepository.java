88cfc27fe78f984aafa8084b6ac4164a24f7f5f1
artur <artur@artur-pc>
2011-12-03 14:22:26 +0400

    Added Service Classes

---
+++ src/main/java/co/javatoday/data/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package co.javatoday.data.repository;
+
+import org.springframework.data.document.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.javatoday.data.model.User;
+
+@Transactional
+public interface UserRepository extends MongoRepository<User, String> {
+	
+}
---
+++ src/main/java/co/javatoday/service/ArticleService.java
@@ -0,0 +1,8 @@
+package co.javatoday.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+
+}
